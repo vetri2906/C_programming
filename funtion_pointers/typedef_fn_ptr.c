3d6449426ecf5a84c02f8777e11fc13352135adc
unknown <vetri290696@gmail.com>
2022-12-18 00:03:57 +0530

    fn_pttr

---
+++ funtion_pointers/typedef_fn_ptr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef int myfunction(int a, int b);
+
+int adder(int a, int b) {
+    return a + b;
+}
+
+int minus(int a, int b) {
+    return a - b;
+}
+
+int callerfunction(myfunction *p, int a, int b) {
+    return p(a, b);
+}
+
+int typedef_fnptr(void) {
+    int result;
+    
+    result = callerfunction(&adder, 15, 20);
+    printf("Add Result: %d\n", result);
+    
+    result = callerfunction(&minus, 15, 20);
+    printf("Substract Result: %d\n", result);
+    
+    return 0;
+}
